ded2f5e916a7de97159f0d019036b5e68a87871d
Tom Liversidge <tom@liversidge.me>
2017-01-19 22:38:20 +0200

    supervise todo lists

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -7,24 +7,28 @@ defmodule Todo.Cache do
   end
 
   def server_process(todo_list_name) do
-    GenServer.call(:todo_cache, {:server_process, todo_list_name})
+    case Todo.Server.whereis(todo_list_name) do
+      :undefined ->
+        # There's no to-do server, so we'll issue request to the cache process.
+        GenServer.call(:todo_cache, {:server_process, todo_list_name})
+
+      pid -> pid
+    end
   end
 
   def init(_) do
-    {:ok, HashDict.new}
+    {:ok, nil}
   end
 
-  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
-    case HashDict.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} ->
-        {:reply, todo_server, todo_servers}
-      :error ->
-        {:ok, new_server} = Todo.Server.start_link(todo_list_name)
-        {
-          :reply,
-          new_server,
-          HashDict.put(todo_servers, todo_list_name, new_server)
-        }
-    end
+  def handle_call({:server_process, todo_list_name}, _, state) do
+      # We need to recheck once again if the server exists.
+      todo_server_pid = case Todo.Server.whereis(todo_list_name) do
+        :undefined ->
+          {:ok, pid} = Todo.ServerSupervisor.start_child(todo_list_name)
+          pid
+
+        pid -> pid
+      end
+      {:reply, todo_server_pid, state}
   end
 end
--- lib/todo/supervisor.ex
+++ lib/todo/supervisor.ex
@@ -9,6 +9,7 @@ defmodule Todo.Supervisor do
     processes = [
       worker(Todo.ProcessRegistry, []),
       supervisor(Todo.Database, ["./persist/"]),
+      supervisor(Todo.ServerSupervisor, []),
       worker(Todo.Cache, [])
     ]
     supervise(processes, strategy: :one_for_one)
